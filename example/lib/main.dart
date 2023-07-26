7324ece9708afb7596453122a020206c71f928e6
canhongchen <1121266119@qq.com>
2023-07-26 10:04:33 +0800

    修改插件名称

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutterplugin/flutterplugin.dart';
+import 'package:tmbleflutterplugin/flutterplugin.dart';
 
 void main() {
   runApp(const MyApp());
